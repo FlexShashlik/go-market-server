70de3e0f068a10a6e322a50170ef70b670957c50
Sevada Saakyan <sevada992@gmail.com>
2019-07-04 18:04:18 +0300

    JWT auth

--- market_server.go
+++ market_server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	"database/sql"
 	"flag"
 	"io"
@@ -9,11 +10,26 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/appleboy/gin-jwt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/logger"
-	"github.com/appleboy/gin-jwt"
 )
+
+// Login struct
+type Login struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
+var identityKey = "id"
+
+// User demo
+type User struct {
+	UserName  string
+	FirstName string
+	LastName  string
+}
 
 // Product struct
 type Product struct {
@@ -23,15 +39,13 @@ type Product struct {
 	ImageExtension string `json:"image_extension"`
 }
 
-const logPath = "server.log"
-
-var verbose = flag.Bool("verbose", false, "print info level logs to stdout")
 var db *sql.DB
 
 func main() {
+	var verbose = flag.Bool("verbose", false, "print info level logs to stdout")
 	flag.Parse()
 
-	lf, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+	lf, err := os.OpenFile("server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
 	if err != nil {
 		logger.Fatalf("Failed to open log file: %v", err)
 	}
@@ -70,6 +84,98 @@ connect:
 
 	router := gin.Default()
 
+	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
+		Realm:       "Flex Market",
+		PrivKeyFile: "jwtRS256.key",
+		PubKeyFile: "jwtRS256pub.key",
+		SigningAlgorithm: "RS512",
+		Timeout:     time.Hour,
+		MaxRefresh:  time.Hour,
+		IdentityKey: identityKey,
+		PayloadFunc: func(data interface{}) jwt.MapClaims {
+			if v, ok := data.(*User); ok {
+				return jwt.MapClaims{
+					identityKey: v.UserName,
+				}
+			}
+			return jwt.MapClaims{}
+		},
+		IdentityHandler: func(c *gin.Context) interface{} {
+			claims := jwt.ExtractClaims(c)
+			return &User{
+				UserName: claims["id"].(string),
+			}
+		},
+		Authenticator: func(c *gin.Context) (interface{}, error) {
+			var loginVals Login
+			if err := c.ShouldBind(&loginVals); err != nil {
+				return "", jwt.ErrMissingLoginValues
+			}
+			userID := loginVals.Username
+			password := loginVals.Password
+
+			if (userID == "admin" && password == "admin") || (userID == "test" && password == "test") {
+				return &User{
+					UserName:  userID,
+					LastName:  "Flexyan",
+					FirstName: "Sevada",
+				}, nil
+			}
+
+			return nil, jwt.ErrFailedAuthentication
+		},
+		Authorizator: func(data interface{}, c *gin.Context) bool {
+			if v, ok := data.(*User); ok && v.UserName == "admin" {
+				return true
+			}
+
+			return false
+		},
+		Unauthorized: func(c *gin.Context, code int, message string) {
+			c.JSON(code, gin.H{
+				"code":    code,
+				"message": message,
+			})
+		},
+		// TokenLookup is a string in the form of "<source>:<name>" that is used
+		// to extract token from the request.
+		// Optional. Default value "header:Authorization".
+		// Possible values:
+		// - "header:<name>"
+		// - "query:<name>"
+		// - "cookie:<name>"
+		// - "param:<name>"
+		TokenLookup: "header: Authorization, query: token, cookie: jwt",
+		// TokenLookup: "query:token",
+		// TokenLookup: "cookie:token",
+
+		// TokenHeadName is a string in the header. Default value is "Bearer"
+		TokenHeadName: "Bearer",
+
+		// TimeFunc provides the current time. You can override it to use another time value. This is useful for testing or if your server uses a different time zone than your tokens.
+		TimeFunc: time.Now,
+	})
+
+	if err != nil {
+		log.Fatal("JWT Error:" + err.Error())
+	}
+
+	router.POST("/login", authMiddleware.LoginHandler)
+
+	router.NoRoute(authMiddleware.MiddlewareFunc(), func(c *gin.Context) {
+		claims := jwt.ExtractClaims(c)
+		log.Printf("NoRoute claims: %#v\n", claims)
+		c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+	})
+
+	auth := router.Group("/auth")
+	// Refresh time can be longer than token timeout
+	auth.GET("/refresh_token", authMiddleware.RefreshHandler)
+	auth.Use(authMiddleware.MiddlewareFunc())
+	{
+		auth.GET("/hello", HelloHandler)
+	}
+
 	v1 := router.Group("/api/v1/")
 	{
 		v1.POST("products/", CreateProduct)
@@ -104,6 +210,18 @@ func UploadImage(product *Product, c *gin.Context) {
 				"message": err.Error()})
 		return
 	}
+}
+
+/* Handlers */
+
+func HelloHandler(c *gin.Context) {
+	claims := jwt.ExtractClaims(c)
+	user, _ := c.Get(identityKey)
+	c.JSON(200, gin.H{
+		"userID":   claims["id"],
+		"userName": user.(*User).UserName,
+		"text":     "Hello World.",
+	})
 }
 
 // CreateProduct creates new product
